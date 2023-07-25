2b4c356e540a95738f3e5a49eebd54384f397a03
NORMAS TECNICAS ESTATALES AMBIENTALES CGCE <ntea.cgce.sma@edomex.gob.mx>
2023-07-25 04:34:06 -0600

    aaa

---
+++ Prueba/Prueba/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Prueba.DAL;
+using Prueba.Models;
+
+namespace Prueba.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MyAppDbContext _context; 
+
+        public ProductController(MyAppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.Include("Categories");
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            LoadCategories();
+            return View();
+        }
+        [NonAction]
+        private void LoadCategories()
+        {
+            var categories = _context.Categories.ToList();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+        }
+        [HttpPost]
+        public IActionResult Create(Product model)
+        {
+            _context.Products.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
